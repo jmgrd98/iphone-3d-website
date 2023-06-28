4d163e480d7e1f1df731d71724936a9f3396539a
João Marcelo Dantas <jmgrd98@gmail.com>
2023-06-28 17:09:58 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import Nav from './components/Nav';
+import Jumbotron from "./components/Jumbotron.jsx";
+import SoundSection from "./components/SoundSection.jsx";
 
 function App() {
 
-  return (
-    <div className="App">
-      Hello
-    </div>
-  );
+    return (
+        <div className="App">
+            <Nav/>
+            <Jumbotron/>
+            <SoundSection/>
+        </div>
+    );
 }
 
 export default App;
